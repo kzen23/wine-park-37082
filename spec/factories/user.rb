8d9d8e2bd5b15b22446662006c9237c08643eb34
kzen23 <94582364+kzen23@users.noreply.github.com>
2022-04-23 09:20:29 +0900

    テストコードのためのインスタンス生成準備
    FactoryBot、Fakerを利用してインスタンスが生成され、テストが実行できるように設定。

---
+++ spec/factories/user.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { Faker::Name.initials(number: 6) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
+    password_confirmation { password }
+    last_name             { "白ブドウ" }
+    first_name            { "泡え"}
+    last_name_kana        { "シロブドウ" }
+    first_name_kana       { "アワエ" }
+    birthday              { "1975-06-21" }
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
     context '新規登録できるときのUserモデル' do
       it "nickname, email, password, password_confirmation, last_name, first_name, last_name_kana, first_name_kana, birthdayに適切な値があれば登録に進める" do
       end
