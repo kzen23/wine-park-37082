1bed07eea15d81d823d494d47bbc81fa91136b91
kzen23 <94582364+kzen23@users.noreply.github.com>
2022-05-01 14:36:27 +0900

    ルーティング設定
    newアクションへのパスを追加。

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'users', to: 'users/registrations#create'
   end
+
   root to: "wine_articles#index"
-  resources :wine_articles, only: :index
+
+  resources :wine_articles, only: [:index, :new]
 end
