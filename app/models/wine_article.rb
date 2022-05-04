0d1b2205c1f1f17b76da8db26d39535e098f8d1b
kzen23 <94582364+kzen23@users.noreply.github.com>
2022-05-04 12:23:06 +0900

    バリデーションの設定

--- app/models/wine_article.rb
+++ app/models/wine_article.rb
@@ -1,4 +1,15 @@
 class WineArticle < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+
+  with_options presence: true do
+    validates :wine_name, length: {maximum: 40}
+    validates :wine_price, numericality: { only_integer: true, message: 'is invalid. Input half width characters' },
+                          length: {maximum: 40}
+    validates :wine_shop, length: {maximum: 40}
+    validates :title, length: {maximum: 40}
+    validates :comment, length: {maximum: 200}
+    validates :wine_type_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :wine_taste_id, numericality: { other_than: 1, message: "can't be blank" }
+  end
 end
