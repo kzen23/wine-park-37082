23ec2b8d12020bdf692c078b333e4087d1596cd5
kzen23 <94582364+kzen23@users.noreply.github.com>
2022-05-04 12:04:29 +0900

    createアクションを定義
    ストロングパラメーターも設定。

--- app/controllers/wine_articles_controller.rb
+++ app/controllers/wine_articles_controller.rb
@@ -5,4 +5,17 @@ class WineArticlesController < ApplicationController
   def new
     @wine_article = WineArticle.new
   end
+
+  def create
+    @wine_article = WineArticle.new(wine_article_params)
+    if @wine_article.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+
+    private
+    def wine_article_params
+      params.require(:wine_article).permit(:wine_name, :wine_name_kana, :wine_price, :wine_shop, :title, :comment, :wine_type_id, :wine_taste_id)
+    end
 end
