eceb4127bd9bb80ff82d710b4200be5e7fe8ee69
kzen23 <94582364+kzen23@users.noreply.github.com>
2022-05-20 22:02:17 +0900

    ワイン記事削除・詳細機能の結合テスト
    ・動作OK。
    ・サポートファイルを使い、記述を簡略化。
    ・メソッド名を一部変更。
    ・rubocop使用。

--- spec/support/confirmation_support.rb
+++ spec/support/confirmation_support.rb
@@ -1,6 +1,6 @@
 module ConfirmaitonSupport
-  def edit_confirmation(wine_article)
-    expect(find('#wine_article_image').value).to eq("")
+  def edit_confirmation(_wine_article)
+    expect(find('#wine_article_image').value).to eq('')
     expect(find('#wine_article_wine_name').value).to eq(@wine_article1.wine_name)
     expect(find('#wine_article_wine_name_kana').value).to eq(@wine_article1.wine_name_kana)
     expect(find('#wine_article_wine_type_id').value.to_i).to eq(@wine_article1.wine_type_id)
@@ -11,43 +11,43 @@ module ConfirmaitonSupport
     expect(find('#wine_article_comment').value).to eq(@wine_article1.comment)
   end
 
-  def input_edit_confirmation(wine_article)
+  def input_edit_confirmation(_wine_article)
     expect(page).to have_selector("img[src$='test_wine2.jpeg']")
-    expect(page).to have_content("Heretat El Padnell")
-    expect(page).to have_content("エレタットエルパドゥルエル")
-    expect(page).to have_content("スパークリングワイン")
-    expect(page).to have_content("極辛口")
+    expect(page).to have_content('Heretat El Padnell')
+    expect(page).to have_content('エレタットエルパドゥルエル')
+    expect(page).to have_content('スパークリングワイン')
+    expect(page).to have_content('極辛口')
     expect(page).to have_content(550)
-    expect(page).to have_content("なんでも酒やカクヤス池尻店")
-    expect(page).to have_content("超コスパスパークリングワイン！！")
-    expect(page).to have_content("550円なのにこの美味さ！！おススメです！！")
+    expect(page).to have_content('なんでも酒やカクヤス池尻店')
+    expect(page).to have_content('超コスパスパークリングワイン！！')
+    expect(page).to have_content('550円なのにこの美味さ！！おススメです！！')
   end
 
   def input_edit_confirmation2
     expect(page).to have_selector("img[src$='test_wine2.jpeg']")
-    expect(page).to have_content("Heretat El Padnell")
-    expect(page).to have_content("")
-    expect(page).to have_content("スパークリングワイン")
-    expect(page).to have_content("極辛口")
+    expect(page).to have_content('Heretat El Padnell')
+    expect(page).to have_content('')
+    expect(page).to have_content('スパークリングワイン')
+    expect(page).to have_content('極辛口')
     expect(page).to have_content(550)
-    expect(page).to have_content("なんでも酒やカクヤス池尻店")
-    expect(page).to have_content("超コスパスパークリングワイン！！")
-    expect(page).to have_content("550円なのにこの美味さ！！おススメです！！")
+    expect(page).to have_content('なんでも酒やカクヤス池尻店')
+    expect(page).to have_content('超コスパスパークリングワイン！！')
+    expect(page).to have_content('550円なのにこの美味さ！！おススメです！！')
   end
 
   def input_edit_confirmation3
     expect(page).to have_selector("img[src$='test_wine.jpeg']")
-    expect(page).to have_content("Heretat El Padnell")
-    expect(page).to have_content("")
-    expect(page).to have_content("スパークリングワイン")
-    expect(page).to have_content("極辛口")
+    expect(page).to have_content('Heretat El Padnell')
+    expect(page).to have_content('')
+    expect(page).to have_content('スパークリングワイン')
+    expect(page).to have_content('極辛口')
     expect(page).to have_content(550)
-    expect(page).to have_content("なんでも酒やカクヤス池尻店")
-    expect(page).to have_content("超コスパスパークリングワイン！！")
-    expect(page).to have_content("550円なのにこの美味さ！！おススメです！！")
+    expect(page).to have_content('なんでも酒やカクヤス池尻店')
+    expect(page).to have_content('超コスパスパークリングワイン！！')
+    expect(page).to have_content('550円なのにこの美味さ！！おススメです！！')
   end
 
-  def input_edit_confirmation4(wine_article)
+  def input_show_confirmation(_wine_article)
     expect(page).to have_selector("img[src$='test_wine.jpeg']")
     expect(page).to have_content(@wine_article1.wine_name)
     expect(page).to have_content(@wine_article1.wine_name_kana)
@@ -59,17 +59,43 @@ module ConfirmaitonSupport
     expect(page).to have_content(@wine_article1.comment)
   end
 
-  def input_index_confirmation(wine_article)
+  def input_show_confirmation2(_wine_article)
+    expect(page).to have_selector("img[src$='test_wine.jpeg']")
+    expect(page).to have_content(@wine_article2.wine_name)
+    expect(page).to have_content(@wine_article2.wine_name_kana)
+    expect(page).to have_content(@wine_article2.wine_type.name)
+    expect(page).to have_content(@wine_article2.wine_taste.name)
+    expect(page).to have_content(@wine_article2.wine_price)
+    expect(page).to have_content(@wine_article2.wine_shop)
+    expect(page).to have_content(@wine_article2.title)
+    expect(page).to have_content(@wine_article2.comment)
+  end
+
+  def input_index_confirmation(_wine_article)
     expect(page).to have_selector("img[src$='test_wine2.jpeg']")
     expect(page).to have_content(@wine_article1.title)
     expect(page).to have_content(@wine_article1.wine_price)
     expect(page).to have_content(@wine_article1.user.nickname)
   end
 
-  def input_index_confirmation2(wine_article)
+  def input_index_confirmation2(_wine_article)
     expect(page).to have_selector("img[src$='test_wine.jpeg']")
     expect(page).to have_content(@wine_article1.title)
     expect(page).to have_content(@wine_article1.wine_price)
     expect(page).to have_content(@wine_article1.user.nickname)
+  end
+
+  def exist_confirmation(_wine_article)
+    expect(page).to have_no_selector("img[src$='test_wine.jpeg']")
+    expect(page).to have_no_content(@wine_article1.title)
+    expect(page).to have_no_content(@wine_article1.wine_price)
+    expect(page).to have_no_content(@wine_article1.user.nickname)
+  end
+
+  def exist_confirmation2(_wine_article)
+    expect(page).to have_no_selector("img[src$='test_wine.jpeg']")
+    expect(page).to have_no_content(@wine_article2.title)
+    expect(page).to have_no_content(@wine_article2.wine_price)
+    expect(page).to have_no_content(@wine_article2.user.nickname)
   end
 end
--- spec/support/sign_in_support.rb
+++ spec/support/sign_in_support.rb
@@ -1,5 +1,5 @@
 module SignInSupport
-  def sign_in(user)
+  def sign_in(_user)
     visit new_user_session_path
     fill_in 'Email', with: @user.email
     fill_in 'Password', with: @user.password
@@ -7,7 +7,7 @@ module SignInSupport
     expect(current_path).to eq(root_path)
   end
 
-  def sign_in_edit(wine_article)
+  def sign_in_edit(_wine_article)
     visit new_user_session_path
     fill_in 'Email', with: @wine_article1.user.email
     fill_in 'Password', with: @wine_article1.user.password
