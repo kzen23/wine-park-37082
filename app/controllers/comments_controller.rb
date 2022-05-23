b083498d62a2684c37d5cf497405019511a06b43
kzen23 <94582364+kzen23@users.noreply.github.com>
2022-05-23 14:45:46 +0900

    commentsコントローラーの編集
    ・edit, updateアクションを定義。
    ・同じ記述をbefore_actionを使い、まとめる。
    ・authenticate_user!でログインしていないユーザーがページに移動することを防止。
    ・check_userで投稿した本人以外、編集出来ないように。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,48 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_user, only: [:edit, :update]
+  before_action :set_wine_article, only: [:new, :create, :edit, :update]
+  before_action :set_comment, only: [:edit, :update]
+
   def new
     @comment = Comment.new
-    @wine_article = WineArticle.find(params[:wine_article_id])
   end
 
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to wine_article_path(@comment.wine_article.id)
-    else
+      redirect_to wine_article_path(params[:wine_article_id])
+    else 
       render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to wine_article_path(params[:wine_article_id])
+    else
+      render 'edit'
     end
   end
 
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, wine_article_id: params[:wine_article_id])
+  end
+
+  def set_wine_article
+    @wine_article =WineArticle.find(params[:wine_article_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def check_user
+    @comment = Comment.find(params[:id])
+    redirect_to root_path unless current_user.id == @comment.user_id
   end
 end
