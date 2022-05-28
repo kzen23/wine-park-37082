f317affd70405e8326083400e66cc28a91afed5a
kzen23 <94582364+kzen23@users.noreply.github.com>
2022-05-28 11:12:22 +0900

    dbファイル
    結合テストでエラーが起きた原因が、commentかもしれないと思い、rollbackしてmy_commentとしたが、関係がない事が分かったのと大幅な修正が必要だと思ったので元に戻す。

--- db/migrate/20220426012013_create_wine_articles.rb
+++ db/migrate/20220426012013_create_wine_articles.rb
@@ -3,13 +3,13 @@ class CreateWineArticles < ActiveRecord::Migration[6.0]
     create_table :wine_articles do |t|
       t.string :wine_name,     null: false
       t.string :wine_name_kana
-      t.integer :wine_price,    null: false
-      t.string :wine_shop,      null: false
-      t.string :title,          null: false
-      t.text :comment,          null: false
-      t.integer :wine_type_id,  null: false
-      t.integer :wine_taste_id, null: false
-      t.references :user,       null: false, foreign_key: true
+      t.integer :wine_price,       null: false
+      t.string :wine_shop,         null: false
+      t.string :title,             null: false
+      t.text    :comment,          null: false
+      t.integer :wine_type_id,     null: false
+      t.integer :wine_taste_id,    null: false
+      t.references :user,          null: false, foreign_key: true
       t.timestamps
     end
   end
