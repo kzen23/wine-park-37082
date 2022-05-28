11aaef9075db388339a82e6d570ecf0229eca076
kzen23 <94582364+kzen23@users.noreply.github.com>
2022-05-28 21:20:54 +0900

    修正
    ・カラム名を変更したので必要な箇所を修正。

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,4 @@
 class Relationship < ApplicationRecord
-  belongs_to :follows_user_id, class_name: "User"
-  belongs_to :followed_user_id, class_name: "User"
+  belongs_to :user_follows_id, class_name: "User"
+  belongs_to :user_followed_id, class_name: "User"
 end
