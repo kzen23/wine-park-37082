f39ce1db2d71bb75dbb098853c66e5cd4a2df4c0
kzen23 <94582364+kzen23@users.noreply.github.com>
2022-05-28 11:14:20 +0900

    コメントの編集・削除の結合テスト
    ・動作OK。
    ・簡潔な記述へ変更
    ・RSpecでは、関連付けをidではなくオブジェクトを対象にした方が良いと知ったのでそのように修正。

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -8,8 +8,8 @@ end
 
 RSpec.describe "Comments", type: :system do
   before do
-    @wine_article = FactoryBot.create(:wine_article)
-    @comment = FactoryBot.build(:comment)
+    @wine_article = create(:wine_article)
+    @comment = build(:comment, user: @wine_article.user, wine_article: @wine_article)
     sleep 0.1
   end
 
@@ -85,22 +85,103 @@ end
 
 RSpec.describe "Comments", type: :system do
   before do
-    @wine_article1 = FactoryBot.create(:wine_article)
-    @wine_article2 = FactoryBot.create(:wine_article)
-    @comment1 = FactoryBot.create(:comment)
-    @comment2 = FactoryBot.create(:comment)
+    @wine_article1 = create(:wine_article)
+    @comment1 = create(:comment, user: @wine_article1.user, wine_article: @wine_article1)
+    @wine_article2 = create(:wine_article)
+    @comment2 =create(:comment, user: @wine_article2.user, wine_article: @wine_article2)
     sleep 0.1
   end
 
   describe 'コメント編集機能' do
     context 'コメント編集ができるとき' do
       it 'ログインしたユーザーは過去に自分が投稿したコメント内容を編集できる' do
-      end
+      # Basic認証を通過する
+      basic_auth root_path
+      # トップページに移動したことを確認する
+      expect(current_path).to eq(root_path)
+      # ログインする
+      sign_in_edit(@wine_article1.user)
+      # 投稿したワイン記事１をクリックする
+      find_link(all('.link1')[@wine_article1.id], href: wine_article_path(@wine_article1.id)).click
+      # 詳細ページに移動した事を確認する
+      expect(current_path).to eq wine_article_path(@wine_article1.id)
+      # コメント一覧に編集のリンクがあることを確認する
+      expect(page).to have_link '編集', href: edit_wine_article_comment_path(@wine_article1, @comment1)
+      # 編集をクリックする
+      click_on '編集'
+      # 編集ページに遷移したことを確認する
+      expect(current_path).to eq(edit_wine_article_comment_path(@wine_article1, @comment1))
+      # 過去の投稿が表示されている事を確認する
+      expect(page).to have_content(@comment1.text)
+      # コメント内容を書き換える
+      fill_in 'comment_text', with: '美味しそうですね！'
+      # 更新するをクリックする
+      click_on '更新する'
+      # データベースの内容が更新されたことを確認する
+      expect(@comment1.reload.text).to eq(@comment1.text)
+    end
     end
     context 'コメント編集ができないとき' do
       it 'ログインしていなければユーザーはコメント編集ができない' do
+      # Basic認証を通過する
+      basic_auth root_path
+      # トップページに移動したことを確認する
+      expect(current_path).to eq(root_path)
+      # 詳細ページへのリンクがないことを確認する
+      expect(page).to have_no_link (all('.link1')[@wine_article1.id]), href: wine_article_path(@wine_article1.id) 
+      expect(page).to have_no_link (all('.link1')[@wine_article2.id]), href: wine_article_path(@wine_article2.id) 
+      # 詳細ページへ直接移動を試みる
+      visit wine_article_path(@wine_article1.id)
+      # ログイン画面に遷移した事を確認する
+      expect(current_path).to eq(new_user_session_path)
+      # 編集ページへの直接移動を試みる1
+      visit edit_wine_article_comment_path(@wine_article1, @comment1)
+      # ログイン画面に移動したことを確認する
+      expect(current_path).to eq(new_user_session_path)
+      # 編集ページへの直接移動を試みる2
+      visit edit_wine_article_comment_path(@wine_article2, @comment2)
+      # ログイン画面に移動したことを確認する
+      expect(current_path).to eq(new_user_session_path)
       end
       it 'ログインしていても自分が投稿したコメント以外は編集ができない' do
+      # Basic認証を通過する
+      basic_auth root_path
+      # トップページに移動したことを確認する
+      expect(current_path).to eq(root_path)
+      # ログインする
+      sign_in_edit(@wine_article1.user)
+      # 投稿したワイン記事２をクリックする
+      find_link(all('.link1')[@wine_article2.id], href: wine_article_path(@wine_article2.id)).click
+      # ワイン記事２の詳細ページに移動したことを確認する
+      expect(current_path).to eq(wine_article_path(@wine_article2.id))
+      # 編集ページへのリンクがないことを確認する
+      expect(page).to have_no_link '編集', href: edit_wine_article_comment_path(@wine_article2, @comment2)
+      end
+      it 'ログインしていて自分が投稿したコメントでも値が空では編集できない' do
+      # Basic認証を通過する
+      basic_auth root_path
+      # トップページに移動したことを確認する
+      expect(current_path).to eq(root_path)
+      # ログインする
+      sign_in_edit(@wine_article1.user)
+      # 投稿したワイン記事１をクリックする
+      find_link(all('.link1')[@wine_article1.id], href: wine_article_path(@wine_article1.id)).click
+      # 詳細ページに移動した事を確認する
+      expect(current_path).to eq wine_article_path(@wine_article1.id)
+      # コメント一覧に編集のリンクがあることを確認する
+      expect(page).to have_link '編集', href: edit_wine_article_comment_path(@wine_article1, @comment1)
+      # 編集をクリックする
+      click_on '編集'
+      # 編集ページに遷移したことを確認する
+      expect(current_path).to eq(edit_wine_article_comment_path(@wine_article1, @comment1))
+      # 過去の投稿が表示されている事を確認する
+      expect(page).to have_content(@comment1.text)
+      # コメント内容を空にする
+      fill_in 'comment_text', with: ""
+      # 更新するをクリックする
+      click_on '更新する'
+      # 保存に失敗したので、編集ページにとどまったままなことを確認する
+      expect(current_path).to eq(wine_article_comment_path(@wine_article1, @comment1))
       end
     end
   end
@@ -108,22 +189,67 @@ end
 
 RSpec.describe "Comments", type: :system do
   before do
-    @wine_article1 = FactoryBot.create(:wine_article)
-    @wine_article2 = FactoryBot.create(:wine_article)
-    @comment1 = FactoryBot.create(:comment)
-    @comment2 = FactoryBot.create(:comment)
+    @wine_article1 = create(:wine_article)
+    @wine_article2 = create(:wine_article)
+    @comment1 = create(:comment, user: @wine_article1.user, wine_article: @wine_article1)
+    @comment2 = create(:comment, user: @wine_article2.user, wine_article: @wine_article2)
     sleep 0.1
   end
 
   describe 'コメント削除機能' do
     context 'コメント削除ができるとき' do
       it 'ログインしたユーザーは過去に自分が投稿したコメント内容を削除できる' do
+      # Basic認証を通過する
+      basic_auth root_path
+      # トップページに移動したことを確認する
+      expect(current_path).to eq(root_path)
+      # ログインする
+      sign_in_edit(@wine_article1.user)
+      # 投稿したワイン記事１をクリックする
+      find_link(all('.link1')[@wine_article1.id], href: wine_article_path(@wine_article1.id)).click
+      # 詳細ページに移動した事を確認する
+      expect(current_path).to eq wine_article_path(@wine_article1.id)
+      # 削除のリンクがあることを確認する
+      expect(page).to have_link '削除', href: wine_article_comment_path(@wine_article1, @comment1)
+      # 削除をクリックするとモデルのカウントが１下がる事を確認する
+      expect{ click_on '削除' }.to change { Comment.count }.by(-1)
+      # ワイン記事１の詳細ページに移動したことを確認する
+      expect(current_path).to eq(wine_article_path(@wine_article1.id))
+      # ワイン記事１の詳細ページに過去に投稿した自分のコメントがない事を確認する
+      expect(page).to have_no_content(@comment1.text)
       end
     end
     context 'コメント削除ができないとき' do
       it 'ログインしていなければユーザーはコメント削除できない' do
+      # Basic認証を通過する
+      basic_auth root_path
+      # トップページに移動したことを確認する
+      expect(current_path).to eq(root_path)
+      # 詳細ページへのリンクがないことを確認する
+      expect(page).to have_no_link (all('.link1')[@wine_article1.id]), href: wine_article_path(@wine_article1.id) 
+      expect(page).to have_no_link (all('.link1')[@wine_article2.id]), href: wine_article_path(@wine_article2.id) 
+      # 詳細ページへ直接移動を試みる1
+      visit wine_article_path(@wine_article1.id)
+      # ログイン画面に移動したことを確認する
+      expect(current_path).to eq(new_user_session_path)
+      # 詳細ページへ直接移動を試みる2
+      visit wine_article_path(@wine_article2.id)
+      # ログイン画面に移動したことを確認する
+      expect(current_path).to eq(new_user_session_path)
       end
       it 'ログインしていても自分が投稿したコメント以外は削除できない' do
+      # Basic認証を通過する
+      basic_auth root_path
+      # トップページに移動したことを確認する
+      expect(current_path).to eq(root_path)
+      # ログインする
+      sign_in_edit(@wine_article1.user)
+      # 投稿したワイン記事２をクリックする
+      find_link(all('.link1')[@wine_article2.id], href: wine_article_path(@wine_article2.id)).click
+      # ワイン記事２の詳細ページに移動したことを確認する
+      expect(current_path).to eq(wine_article_path(@wine_article2.id))
+      # 削除へのリンクがないことを確認する
+      expect(page).to have_no_link '削除', href: wine_article_comment_path(@wine_article2, @comment2)
       end
     end
   end
