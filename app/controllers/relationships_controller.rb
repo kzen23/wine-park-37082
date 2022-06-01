6ab7b19c8746bb72f1ae2cb4650c6e5d334862e0
kzen23 <94582364+kzen23@users.noreply.github.com>
2022-06-01 11:14:50 +0900

    relationshipsコントローラーの編集
    ・create, destroyメソッド作成。
    ・一覧表示で使うためのfollowings、followersメソッドを作成。

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,24 @@
 class RelationshipsController < ApplicationController
-  def followed
+  before_action :authenticate_user!
+
+  def create
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
   end
 
-  def follows
+  def destroy
+    follow = current_user.relationships.find_by(user_followed_id: params[:user_id])
+    follow.destroy
+    redirect_to request.referer
+  end
+
+  def followings
+    @user = User.find(params[:user_id])
+    @users = @user.followings
+  end
+
+  def followers
+    @user = User.find(params[:user_id])
+    @users = @user.followers
   end
 end
