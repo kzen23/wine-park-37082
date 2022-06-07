f53f196659d143eeefee4aadf04d29c890980af5
kzen23 <94582364+kzen23@users.noreply.github.com>
2022-06-07 18:42:11 +0900

    showアクションを定義

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,9 @@
 class FavoritesController < ApplicationController
+  def show
+    @user = WineArticle.user.find(params[:wine_article_id])
+    @favorite_wine_articles = @user.favorite_wine_articles
+  end
+
   def create
     favorite = current_user.favorites.build(wine_article_id: params[:wine_article_id])
     favorite.save
