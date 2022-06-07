c4ebb5103a06186d2a03bb0933532d46aba15109
kzen23 <94582364+kzen23@users.noreply.github.com>
2022-06-07 18:28:07 +0900

    favoriteコントローラー編集
    create, destroyアクションを定義。

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,13 @@
 class FavoritesController < ApplicationController
   def create
+    favorite = current_user.favorites.build(wine_article_id: params[:wine_article_id])
+    favorite.save
+    redirect_to wine_article_path(params[:wine_article_id])
   end
 
   def destroy
+    favorite = Favorite.find_by(wine_article_id: params[:wine_article_id], user_id: current_user.id)
+    favorite.destroy
+    redirect_to wine_article_path(params[:wine_article_id])
   end
 end
