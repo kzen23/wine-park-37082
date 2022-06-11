6a0c0c7de538f406c20e805e154a80a44f27a4e9
kzen23 <94582364+kzen23@users.noreply.github.com>
2022-06-11 22:32:03 +0900

    いいね機能モデル単体テストを実行

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ワイン投稿記事といいね' do
+    before do
+      @favorite = create(:favorite)
+    end
+    context 'ワイン投稿記事が削除された時' do
+      it 'ワイン投稿記事が削除されるといいねも同時に削除される' do
+        expect { @favorite.wine_article.destroy }.to change { Favorite.count }.by(-1)
+      end
+    end
+  end
 end
